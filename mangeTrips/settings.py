7a29ef9ad59802836970bed660ab4d363eed442b
Desenvolvimento <dsadm@educ501.local>
2024-04-08 19:53:04 -0300

    first commit

--- mangeTrips/settings.py
+++ mangeTrips/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 
 from datetime import timedelta
 
+import os
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bj10r+p3+(_fmwpvdhngccan_2#!2)rfw*v7dwsp=_v5^dq4qp'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
@@ -84,7 +89,7 @@ WSGI_APPLICATION = 'mangeTrips.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': env('ENGINE'),
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
